ef88cac46c1846094622c774cae71cbc815053cb
Howard Huang <howardhuang96@gmail.com>
2016-09-22 19:35:33 -0700

    functionize the handlebars grab

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-$(document).ready(function() {
+function loadArticle(dayNumber){
   $.ajax({
     dataType: "json",
     url: "https://spreadsheets.google.com/feeds/list/1t52h4iKh2TaUkzeZZLqX3J8QEcCg-Wn1Ku2UNQISaFQ/od6/public/values?alt=json",
     success: function(data) {
       data = data.feed.entry;
       console.log(data);
+
+
+      
     }
   });
+}
+
+
+$(document).ready(function() {
+
 });
